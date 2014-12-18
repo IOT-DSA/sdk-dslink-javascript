f4eb3bf332d597a8a49ecd5790d370e5fb69d7a9
Michael Bullington <mikebullingtn@gmail.com>
2014-12-18 18:36:40 -0500

    Not busy anymore, so tables.
    - Implemented the two table types implemented in the Dart SDK.
    - Implemented a nice toString() function for TimeRange.
    - Fixed small things?

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = _.merge(
   require('./lib/value.js'),
   require('./lib/node.js'),
   require('./lib/rollup.js'),
+  require('./lib/table.js'),
   require('./lib/trends.js'),
   require('./lib/util.js'));
