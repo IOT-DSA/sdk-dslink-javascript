31c26eab2d53755b3d4f0530f5196e86f0fab893
Michael Bullington <mikebullingtn@gmail.com>
2014-12-20 22:48:24 -0500

    Node.toMap(), Action.toMap(), Value is a map.
    - Added Node.toMap() and Action.toMap(), which output to a JSON-like map that goes with the draft nodeAPI specification.
    - Added more variables to Node.
    - Changing Value.value now checks to see if it's a different value before settings.
    - Fixed some things in value. I didn't read the draft right...

---
+++ examples/node.js
@@ -0,0 +1,18 @@
+var DS = require('../index.js');
+
+var node = new DS.Node("Hello");
+node.value = new DS.Value("Hello world!");
+
+var action = new DS.Action("One", {
+  'params': {
+  	'one': DS.ValueType.STRING,
+  	'two': DS.ValueType.STRING
+  },
+  'results': {
+  	'three': DS.ValueType.STRING,
+  	'four': DS.ValueType.STRING
+  },
+});
+node.addAction(action);
+
+console.log(JSON.stringify(node.toMap()));
