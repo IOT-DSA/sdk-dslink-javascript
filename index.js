002a546523621e779207c200c88436412f6387bc
Michael Bullington <mikebullingtn@gmail.com>
2015-01-16 22:36:38 -0500

    Work on connections, etc.
    - Added attributes to nodes, quick thing.
    - Worked on Connection, started WebSocketConnection.
    - Split link.js code into Link, Connection, and starting to split into WebSocketConnection.
    - Misc.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var _ = require("./lib/internal.js");
 
 module.exports = _.mixin({},
+  require('./lib/connection/connection.js'),
+  require('./lib/connection/websocket.js'),
   require('./lib/responder/responder.js'),
   require('./lib/value.js'),
   require('./lib/node.js'),
