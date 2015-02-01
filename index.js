cb5b099bf61f848520dab5edadef6dae21a04c6e
Michael Bullington <mikebullingtn@gmail.com>
2015-01-31 23:49:42 -0500

    Implement streams, more reorganization.
    - Stream implementation. Pretty straight forward, handles errors, stream state, etc.
    - Rewrote a lot of Client and Responder.
    - Reorganized into client/ and server/ folders. Should be easier to manage now.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var _ = require('./lib/internal.js');
 
 module.exports = _.mixin({},
-  require('./lib/connection/websocket.js'),
-  require('./lib/connection/http.js'),
-  require('./lib/link/link.js'),
+  require('./lib/connection/error.js'),
+  require('./lib/connection/client/websocket.js'),
+  require('./lib/connection/client/http.js'),
+  require('./lib/connection/server/websocket.js'),
+  require('./lib/connection/server/http.js'),
   require('./lib/responder/responder.js'),
+  require('./lib/link/link.js'),
   require('./lib/value.js'),
   require('./lib/node.js'),
   require('./lib/util.js'));
