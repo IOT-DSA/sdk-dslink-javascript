9c439f9793ad49a568897165ef1b57e824808ee7
mbullington <mikebullingtn@gmail.com>
2015-02-28 20:43:00 -0500

    Add tests, fix bugs, clean up.
    - Added tests for about half of Node.
    - Cleaned up subscribe/unsubscribe test.
    - Fixed bugs found through tests.
    - Some Node code cleanup.
    - Added Node.config().

--- test/test.js
+++ test/test.js
@@ -4,33 +4,35 @@ var assert = require('assert'),
     _ = require('../lib/internal/util.js');
 
 describe('Value', function() {
-  it('correctly matches options', function() {
-    var date = new Date();
-    var value = new DS.Value(null, {
-      timestamp: date,
-      type: DS.ValueType.BOOLEAN
+  describe('constructor', function() {
+    it('correctly matches options', function() {
+      var date = new Date();
+      var value = new DS.Value(null, {
+        timestamp: date,
+        type: DS.ValueType.BOOLEAN
+      });
+
+      assert(value.timestamp === date);
+      assert(value.type === DS.ValueType.BOOLEAN);
     });
 
-    assert(value.timestamp === date);
-    assert(value.type === DS.ValueType.BOOLEAN);
-  });
+    it('correctly identifies type', function() {
+      assert(new DS.Value(true).type === DS.ValueType.BOOLEAN);
+      assert(new DS.Value('').type === DS.ValueType.STRING);
+      assert(new DS.Value(new String('')).type === DS.ValueType.STRING);
+      assert(new DS.Value(1).type === DS.ValueType.NUMBER);
+      assert(new DS.Value(new Number(1)).type === DS.ValueType.NUMBER);
+      assert(new DS.Value(null).type === DS.ValueType.NULL);
+      assert(new DS.Value(undefined).type === DS.ValueType.NULL);
+    });
 
-  it('correctly identifies type', function() {
-    assert(new DS.Value(true).type === DS.ValueType.BOOLEAN);
-    assert(new DS.Value('').type === DS.ValueType.STRING);
-    assert(new DS.Value(new String('')).type === DS.ValueType.STRING);
-    assert(new DS.Value(1).type === DS.ValueType.NUMBER);
-    assert(new DS.Value(new Number(1)).type === DS.ValueType.NUMBER);
-    assert(new DS.Value(null).type === DS.ValueType.NULL);
-    assert(new DS.Value(undefined).type === DS.ValueType.NULL);
-  });
-
-  it('is immutable', function() {
-    try {
-      var value = new DS.Value(true);
-      value.value = false;
-      assert(false);
-    } catch(e) {}
+    it('is immutable', function() {
+      try {
+        var value = new DS.Value(true);
+        value.value = false;
+        assert(false);
+      } catch(e) {}
+    });
   });
 
   it('isValid()', function() {
@@ -76,6 +78,139 @@ describe('NodeProvider', function() {
     provider.addNode('/one');
 
     assert(!_.isNull(provider.root.children['one']));
+
+    try {
+      provider.addNode('/');
+      assert(false);
+    } catch(e) {}
+
+    try {
+      provider.addNode('/one');
+      assert(false);
+    } catch(e) {}
+  });
+});
+
+describe('Node', function() {
+  it('constructor', function() {
+    var node = new DS.Node('test');
+    assert(node.id === node.name);
+
+    node = new DS.Node('test', {
+      '$name': 'pizza',
+      '@hello': 'hello world!',
+      '?value': 2,
+      '$type': 'bool'
+    });
+
+    assert(node.id === 'test');
+    assert(node.name === 'pizza');
+    assert(node.attribute('hello') === 'hello world!');
+    assert(node.value.value === 2);
+    assert(node.type === 'number');
+
+    node = new DS.Node('test', {
+      '?value': new DS.Value(2)
+    });
+
+    assert(node.value.value === 2);
+    assert(node.type === 'number');
+  });
+
+  it('config properties', function() {
+    var node = new DS.Node('test');
+
+    assert(node.name === node.__priv__.config.name);
+    assert(node.profile === node.__priv__.config.is);
+    assert(node.type === node.__priv__.config.type);
+  });
+
+  it('value', function() {
+    var node = new DS.Node('test');
+
+    try {
+      node.value = true;
+      assert(false);
+    } catch(e) {}
+
+    var node = new DS.Node('test', {
+      '?value': true
+    });
+
+    try {
+      node.value = 0;
+      assert(false);
+    } catch(e) {}
+
+    node.value = false;
+    assert(node.value.value === false);
+
+    node.value = new DS.Value(true);
+    assert(node.value.value === true);
+  });
+
+  it('attribute()', function() {
+    var node = new DS.Node('test', {
+      '@one': 1,
+      '@two': 2
+    });
+
+    assert(_.typeOf(node.attribute()) === 'object');
+
+    assert(node.attribute('one') === 1);
+    assert(node.attribute('two') === 2);
+
+    node.attribute('one', 2);
+    node.attribute('two', 3);
+
+    assert(node.attribute('one') === 2);
+    assert(node.attribute('two') === 3);
+
+    node.attribute({
+      one: 3,
+      two: 4
+    });
+
+    assert(node.attribute('one') === 3);
+    assert(node.attribute('two') === 4);
+
+    node.attribute('three', 3);
+    assert(node.attribute('three') === 3);
+
+    node.attribute('three', null);
+    assert(Object.keys(node.attribute()).indexOf('three') === -1);
+  });
+
+  // fun with tests
+  it('config()', function() {
+    var node = new DS.Node('test', {
+      '$name': 'pizza'
+    });
+
+    assert(_.typeOf(node.config()) === 'object');
+
+    assert(node.config('name') === 'pizza');
+    assert(node.config('is') === 'node');
+
+    node.config('name', 'ice cream');
+    node.config('is', 'io.js');
+
+    assert(node.config('name') === 'ice cream');
+    assert(node.config('is') === 'io.js');
+
+    node.config({
+      name: 'mountain dew',
+      is: 'rhino'
+    });
+
+    assert(node.config('name') === 'mountain dew');
+    assert(node.config('is') === 'rhino');
+
+    node.config('invokable', 'read');
+    assert(node.config('invokable') === 'read');
+
+    node.config('invokable', null);
+    assert(Object.keys(node.config()).indexOf('invokable') === -1);
   });
 });
 
@@ -95,7 +230,8 @@ describe('Method', function() {
   it('invoke', function(done) {
     var invoked = false;
     var action = new DS.Action(function(node, params) {
-      invoked = true;
+      // make sure it's only called once
+      invoked = !invoked;
     });
 
     provider.root.load({
@@ -123,34 +259,74 @@ describe('Method', function() {
     });
   });
 
-  it('subscribe', function(done) {
+  it('subscribe/unsubscribe', function(done) {
     responder.client.done();
     responder.client.start();
     provider.root.load({
       test: {
-        '?value': new DS.Value(true)
+        '?value': true
       }
     });
 
-    responder.client.once('send', function(msg) {
-      var sub = msg.responses[0];
-      var res = msg.responses[1];
+    var count = 0;
+    var subscribeHandler = function(msg) {
+      count++;
 
-      assert(sub.rid === 0);
-      assert(sub.stream === 'open');
+      if(count === 1) {
+        var sub = msg.responses[0];
+        var res = msg.responses[1];
 
-      var update = sub.updates[0];
+        assert(sub.rid === 0);
+        assert(sub.stream === 'open');
 
-      assert(update.path === '/test');
-      assert(update.value === true);
-      assert(!_.isNull(update.timestamp));
+        var update = sub.updates[0];
 
-      assert(res.rid === 2);
-      assert(res.stream === 'closed');
+        assert(update.path === '/test');
+        assert(update.value === true);
+        assert(!_.isNull(update.ts));
 
-      responder.client.done();
-      done();
-    });
+        assert(res.rid === 2);
+        assert(res.stream === 'closed');
+
+        provider.getNode('/test').value = false;
+      }
+
+      if(count === 2) {
+        var sub = msg.responses[0];
+
+        assert(sub.rid === 0);
+        assert(sub.stream === 'open');
+
+        var update = sub.updates[0];
+
+        assert(update.path === '/test');
+        assert(update.value === false);
+        assert(!_.isNull(update.ts));
+
+        responder.client.receiveMessage({
+          requests: [{
+            rid: 3,
+            method: 'unsubscribe',
+            paths: ['/test']
+          }]
+        });
+      }
+
+      if(count === 3) {
+        var res = msg.responses[0];
+
+        assert(res.rid === 3);
+        assert(res.stream === 'closed');
+        assert(Object.keys(responder.client.streamResponse(0).nodes).length === 0);
+
+        responder.client.removeListener('send', subscribeHandler);
+        responder.client.done();
+        done();
+      }
+    };
+
+    responder.client.on('send', subscribeHandler);
+
     responder.client.receiveMessage({
       requests: [{
         rid: 2,
