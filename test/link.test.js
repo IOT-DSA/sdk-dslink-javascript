25cf10e2c229374dfade5a34619de1bb157a82d1
mbullington <mikebullingtn@gmail.com>
2015-03-25 22:25:42 -0400

    Add more tests, fix some bugs.

--- test/link.test.js
+++ test/link.test.js
@@ -7,21 +7,20 @@ var assert = require('assert'),
 if(!_.isNull(DS.Link)) {
   var mock = require('mock' + '-fs');
   var path = require('pa' + 'th');
+  var fs = require('f' + 's');
 
   describe('Link', function() {
     before(function() {
       var map = {};
 
       map[path.resolve(process.cwd(), 'dslink.json')] = JSON.stringify({
-        configs: {
-
-        }
+        configs: {}
       });
 
       mock(map);
     });
 
-    it('works', function() {
+    it('generates files correctly', function() {
       var provider = new DS.NodeProvider();
       var link = new DS.Link('test', provider);
 
@@ -32,6 +31,16 @@ if(!_.isNull(DS.Link)) {
       });
 
       link.save();
+
+      var readdir = fs.readdirSync(process.cwd());
+      assert(readdir.indexOf('.dslink.key') >= 0 && readdir.indexOf('ds_nodes.json') >= 0);
+    });
+
+    it('loads node structure', function() {
+      var provider = new DS.NodeProvider();
+      var link = new DS.Link('test', provider);
+
+      assert(!_.isNull(provider.children.hello));
     });
   });
 }
