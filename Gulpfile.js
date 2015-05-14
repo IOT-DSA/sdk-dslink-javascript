725795f7bf2f4813778a4835211263606701b63e
mbullington <mikebullingtn@gmail.com>
2015-05-13 20:44:53 -0400

    Add $is functionality to addNode.

--- Gulpfile.js
+++ Gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('browser', function() {
       .bundle()
       .pipe(source('dslink.js'))
       .pipe(buffer())
-      .pipe(uglify())
+//      .pipe(uglify())
       .pipe(gulp.dest('dist/'));
   };
 
