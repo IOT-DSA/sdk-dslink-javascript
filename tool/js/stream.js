94bd15bc37159489765934dd416495ae882f0a70
Michael Bullington <mikebullingtn@gmail.com>
2015-06-04 23:32:10 -0400

    Stream Transformer (not part of calzone, made specifically for DSA)
    - Stream Transformer! It's not part of calzone, since it could not work with everything. Like if you had a Stream class somewhere. Or actually wanted to use node Streams. It's just better suited in this repo.
    - Other stuff, probably.

--- tool/js/stream.js
+++ tool/js/stream.js
@@ -2,8 +2,26 @@ var EventEmitter = require('events').EventEmitter;
 
 // aiming for a node-like Stream API, but without the weight
 // isn't really for data, but for just values elapsed over time
-function Stream(dartStream) {
-
+function Stream(dartStream, typeTransform) {
+  dartStream._createSubscription$4({
+    // onData
+    call$1: function(data) {
+      data = typeTransform(data);
+      this.emit('data', data);
+    }
+  },
+  {
+    // onError
+    call$1: function(error) {
+      this.emit('error', error);
+    }
+  }, {
+    // onDone
+    call$0: function() {
+      this.emit('done');
+    }
+  // cancel on error
+  }, true);
 }
 
 Stream.prototype = new EventEmitter();
--- tool/wrapper_gen.dart
+++ tool/wrapper_gen.dart
@@ -15,36 +15,44 @@ class StreamTransformer extends TypeTransformer {
   dynamicTransformTo(StringBuffer output, List<String> globals) {
     if(!globals.contains(_STREAM_PREFIX))
       globals.add(_STREAM_PREFIX);
+    // TODO
   }
 
   @override
   dynamicTransformFrom(StringBuffer output, List<String> globals) {
     if(!globals.contains(_STREAM_PREFIX))
       globals.add(_STREAM_PREFIX);
+    output.write("if(typeof(obj._createSubscription\$4) !== 'undefined') { return new module.exports.Stream(obj, dynamicTo); }");
   }
 
   @override
   transformToDart(StringBuffer output, TypeTransformer base, String name, List tree, List<String> globals) {
     if(!globals.contains(_STREAM_PREFIX))
       globals.add(_STREAM_PREFIX);
+    // TODO
   }
 
   @override
   transformFromDart(StringBuffer output, TypeTransformer base, String name, List tree, List<String> globals) {
     if(!globals.contains(_STREAM_PREFIX))
       globals.add(_STREAM_PREFIX);
+    output.write("$name = new module.exports.Stream($name, function(obj) {");
+    if(tree.length > 1) {
+      base.transformToDart(output, null, "obj", tree[1], globals);
+    }
+    output.write("return obj; });");
   }
 }
 
 main(List<String> args) {
-  var compiler = new Compiler.fromPath(args[0], "temp/dslink.js.info.json");
-  var include = new File("tool/dslink.include").readAsLinesSync().where((line) => line.trim().length > 0 && !line.trim().startsWith("#"));
-
-  compiler.typeTransformers.addAll([
+  var compiler = new Compiler.fromPath(args[0], "temp/dslink.js.info.json", typeTransformers: [
     new CollectionsTransformer(true),
     new PromiseTransformer(true),
-    new ClosureTransformer()
+    new ClosureTransformer(),
+    new StreamTransformer()
   ]);
+
+  var include = new File("tool/dslink.include").readAsLinesSync().where((line) => line.trim().length > 0 && !line.trim().startsWith("#"));
 
   var str = compiler.compile(include);
 
