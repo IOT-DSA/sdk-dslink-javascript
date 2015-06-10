75cd32e8432687268ecee634a4493c3c31a08a02
Kenneth Endfinger <kaendfinger@gmail.com>
2015-06-10 02:07:10 -0400

    Fix Build Script

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
+#!/usr/bin/env bash
 COL_RESET=$'\e[0m'
 
 COL_GREEN=$'\e[1;32m'
 COL_BLUE=$'\e[1;34m'
 
-if [ $2 != "dev" ]; then
+if [ ! -d "packages" ]
+then
+  pub get
+fi
+
+if [ "$2" != "dev" ]
+then
   rm -rf temp dist
 
   mkdir temp
@@ -31,13 +38,13 @@ if [ $2 != "dev" ]; then
   echo "4/9 Fetching node dependencies"
   echo -n "$COL_RESET"
 
-  sudo npm install
+  npm install
 
   echo -n "$COL_BLUE"
   echo "5/9 Building Dart SDK with dart2js"
   echo -n "$COL_RESET"
 
-  dart2js --dump-info --enable-experimental-mirrors -o temp/dslink.js tool/$1_stub.dart
+  dart2js --dump-info --enable-experimental-mirrors -o temp/dslink.js "tool/$1_stub.dart"
 else
   echo -n "$COL_GREEN"
   echo "Dev build! Skipping the first 5 steps."
@@ -49,29 +56,30 @@ echo -n "$COL_BLUE"
 echo "6/9 Generating JS wrapper for Dart SDK"
 echo -n "$COL_RESET"
 
-dart tool/wrapper_gen.dart tool/$1_stub.dart > temp/wrapper.js
+dart tool/wrapper_gen.dart "tool/$1_stub.dart" > temp/wrapper.js
 
 echo -n "$COL_BLUE"
 echo "7/9 Patching JS wrapper into dart2js output"
 echo -n "$COL_RESET"
 
-pub run calzone:calpatcher -t $1 -f temp/dslink.js -w temp/wrapper.js  > temp/dslink.patched.js
+pub run calzone:calpatcher -t "$1" -f temp/dslink.js -w temp/wrapper.js  > temp/dslink.patched.js
 
 echo -n "$COL_BLUE"
 echo "8/9 Generating JS SDK at dist/dslink.js"
 echo -n "$COL_RESET"
 
-if [ $1 == "browser" ]; then
-  node node_modules/browserify/bin/cmd.js temp/dslink.patched.js --standalone DS > dist/dslink.$1.js
+if [ "$1" == "browser" ]
+then
+  node node_modules/browserify/bin/cmd.js temp/dslink.patched.js --standalone DS > "dist/dslink.$1.js"
 else
-  cat temp/dslink.patched.js > dist/dslink.$1.js
+  cat temp/dslink.patched.js > dist/"dslink.$1.js"
 fi
 
 echo -n "$COL_BLUE"
 echo "9/9 Minifing at dist/dslink.min.js"
 echo -n "$COL_RESET"
 
-node node_modules/uglify-js/bin/uglifyjs dist/dslink.$1.js > dist/dslink.$1.min.js
+node node_modules/uglify-js/bin/uglifyjs "dist/dslink.$1.js" > "dist/dslink.$1.min.js"
 
 echo -n "$COL_GREEN"
 echo "Done!"
