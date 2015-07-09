1bd4857a0db038ed186853e686177ffe5a74ffa0
mbullington <mikebullingtn@gmail.com>
2015-07-09 10:33:22 -0400

    Update grind.dart to include mixins.

--- tool/grind.dart
+++ tool/grind.dart
@@ -48,6 +48,8 @@ class DSLinkBuilder extends Builder {
 
   Future<String> build() async {
     var output = await super.build();
+    output += new File("tool/js/mixin.js").readAsStringSync();
+
     if(stage == BuilderStage.COMPILE)
       return null;
 
