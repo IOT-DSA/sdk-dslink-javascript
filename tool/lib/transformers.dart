030641189de986b57da51dcf4b4ded17f79dcefe
Michael Bullington <mikebullingtn@gmail.com>
2015-07-24 01:02:52 -0400

    Fix streams.

--- tool/lib/transformers.dart
+++ tool/lib/transformers.dart
@@ -10,37 +10,36 @@ class StreamTransformer extends TypeTransformer {
 
   @override
   transformFromDart(Compiler compiler, StringBuffer output) {
-    var c = compiler.classes["dart.async._ControllerStream"];
+    var c = compiler.classes["dslink.utils.CachedStreamWrapper"];
     compiler.globals.add("""
     var EventEmitter = require('events').EventEmitter;
 
     // aiming for a node-like Stream API, but without the weight
     // isn't really for data, but for just values elapsed over time
     function Stream(dartStream) {
-      dartStream.${c.key.getMangledName("_createSubscription")}({
+      dartStream.${c.key.getMangledName("listen")}({
         // onData
         ${compiler.isMinified ? "\$1" : "call\$1"}: function(data) {
           this.emit('data', dynamicFrom(data));
         }.bind(this)
-      },
-      {
-        // onError
-        ${compiler.isMinified ? "\$1" : "call\$1"}: function(error) {
-          this.emit('error', error);
-        }.bind(this)
-      }, {
+      }, true, {
         // onDone
         ${compiler.isMinified ? "\$0" : "call\$0"}: function() {
           this.emit('done');
         }.bind(this)
       // cancel on error
-      }, true);
+      }, {
+        // onError
+        ${compiler.isMinified ? "\$1" : "call\$1"}: function(error) {
+          this.emit('error', error);
+        }.bind(this)
+      });
     }
 
     Stream.prototype = new EventEmitter();
 
     module.exports.Stream = Stream;
     """);
-    output.write("if(obj.${c.key.getMangledName("_createSubscription")}) { return new module.exports.Stream(obj); }");
+    output.write("if(obj.${c.key.getMangledName("listen")}) { return new module.exports.Stream(obj); }");
   }
 }
