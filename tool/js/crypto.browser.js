d551a050a69c496b07f9ef8d7b45b5833ff57952
Michael Bullington <mikebullingtn@gmail.com>
2015-10-20 19:54:25 -0400

    Update crypto.browser.js

--- tool/js/crypto.browser.js
+++ tool/js/crypto.browser.js
@@ -3,7 +3,7 @@ var sjcl = require('dhcurve/vendor/sjcl');
 module.exports = {
   randomBytes: function() {
     var buf = new Buffer(4);
-    buf.writeUInt32BE(sjcl.random.randomWords(1, 0)[0]);
+    buf.writeUInt32BE(sjcl.random.randomWords(1, 0)[0]>>>0);
     return buf;
   },
   createHash: require('sha.js')
